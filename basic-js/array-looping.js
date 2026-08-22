645ec0c4bfdf1c6b1bc0cb723fcd2bd5211ec2a8
joinalpro <joinalzx@gmail.com>
2026-08-22 14:09:09 +0600

    js recap

---
+++ basic-js/array-looping.js
@@ -0,0 +1,27 @@
+const fruits = ['Apple', 'Banana', 'Anar','Water melon']
+
+// console.log(fruits[1])
+
+for(let i = 0; i < fruits.length; i++){
+    // console.log(i)
+    // console.log(fruits[i])
+}
+
+const numbers = [12, 34, 45, 33, 65, 79]
+for(let i = 0; i < numbers.length; i++){
+    // console.log(numbers[i])
+}
+
+let k = 0;
+while (k < fruits.length){
+    // console.log(fruits[k])
+    k++;
+}
+
+const prices = [15, 100, 50, 60, 25]
+
+let n = 0;
+while(n < prices.length){
+    console.log(prices[n])
+    n++;
+}
---
+++ basic-js/revision.js
@@ -0,0 +1,41 @@
+const price = 500;
+
+const name = 'ali khan'
+
+const isPoor = false;
+
+// Primitive type and non primitive type
+
+const friends = ['Elon', 'Mark', 'Bill', 'Jeff']
+
+const student = {
+    class: 9,
+    name: 'Tomi khan',
+    address: 'kochu khet',
+    age: 17
+}
+
+// condition
+if(price > 1000){
+    console.log('too expensive not for me')
+}
+else if(price => 500){
+    console.log('maybe I can buy this')
+}
+else {
+    console.log('within my budget')
+}
+
+// loop
+
+let pushUp = 0;
+while(pushUp < 10){
+    console.log('pushup', pushUp)
+    pushUp++;
+}
+
+// for
+
+for (let i=0; i<10; i++){
+    console.log(i)
+}
